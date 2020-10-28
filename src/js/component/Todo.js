92f960e2f3e45d0ca83db16e91cec3b1d0bb3152
Ernesto <ernestoemilanes@gmail.com>
2020-10-28 15:35:58 -0400

    completed

--- src/js/component/Todo.js
+++ src/js/component/Todo.js
@@ -1,15 +1,59 @@
 import React, { useState, useEffect } from "react";
 
 const Todo = () => {
-	const [input, setInput] = useState("");
 	const [todo, setTodo] = useState([]);
+	const [input, setInput] = useState("");
+	const url =
+		"https://assets.breatheco.de/apis/fake/todos/user/ernestoemilanes123";
 	useEffect(() => {
-		const task = ["task one", "task two"];
-		setTodo(task);
+		fetch(`${url}`)
+			.then(response => response.json())
+			.then(json => setTodo(json));
 	}, []);
 	const addTodo = () => {
-		const newList = [...todo, input];
-		setTodo(newList);
+		const newList = [...todo, { done: false, label: input }];
+		console.log(newList);
+		fetch(`${url}`, {
+			method: "PUT",
+			body: JSON.stringify(newList),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {
+				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then(() => {
+				fetch(`${url}`)
+					.then(response => response.json())
+					.then(json => setTodo(json));
+			})
+			.catch(error => {
+				//error handling
+				console.log(error);
+			});
+	};
+	const deleteTodo = todoIndex => {
+		const deleteList = todo.filter((item, index) => index !== todoIndex);
+		fetch(`${url}`, {
+			method: "PUT",
+			body: JSON.stringify(deleteList),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {
+				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then(() => {
+				fetch(`${url}`)
+					.then(response => response.json())
+					.then(json => setTodo(json));
+			})
+			.catch(error => {
+				//error handling
+				console.log(error);
+			});
 	};
 	return (
 		<div className="todoContainer">
@@ -19,13 +63,15 @@ const Todo = () => {
 				placeholder="Todo"
 			/>
 			<button onClick={() => addTodo()}>Submit</button>
-			{todo.map((items, index) => {
-				return (
-					<div className="todoItems" key={index}>
-						{items}
-					</div>
-				);
-			})}
+			{todo &&
+				todo.map((items, index) => {
+					return (
+						<div className="todoItems" key={index}>
+							{items.label}
+							<span onClick={() => deleteTodo(index)}>X</span>
+						</div>
+					);
+				})}
 		</div>
 	);
 };
